8d359c7e508dd330efccf17b9ee42cb82cfef110
mchooooo <mchooooo@naver.com>
2025-01-20 19:04:06 +0900

    add Todo domain

--- src/main/java/hello/toy/todoapp/todo/domain/Todo.java
+++ src/main/java/hello/toy/todoapp/todo/domain/Todo.java
@@ -2,10 +2,25 @@ package hello.toy.todoapp.todo.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Todo {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "TODO_ID")
     private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "MEMBER_ID", nullable = false)
+//    private Member member;
+
 }
