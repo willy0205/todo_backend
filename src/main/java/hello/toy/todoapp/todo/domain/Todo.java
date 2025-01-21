a7ed7f440ae58bead7ee5b7cc70fc23d906913ec
mchooooo <64193510+mchooooo@users.noreply.github.com>
2025-01-21 14:46:45 +0900

    Todo domain (#2)
    * add Todo domain
    
    * add todo entity, member 엔티티에 투두 추가

--- src/main/java/hello/toy/todoapp/todo/domain/Todo.java
+++ src/main/java/hello/toy/todoapp/todo/domain/Todo.java
@@ -1,11 +1,27 @@
 package hello.toy.todoapp.todo.domain;
 
+import hello.toy.todoapp.member.domain.Member;
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Todo {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "TODO_ID")
     private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID", nullable = false)
+    private Member member;
+
 }
